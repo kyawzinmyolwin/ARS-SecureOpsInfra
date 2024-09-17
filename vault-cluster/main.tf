6774c347803a13b509e444e89f2f53ab3b65ebf4
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-17 22:10:46 +0800

    ars-vault-cluster 1st commit

---
+++ vault-cluster/main.tf
@@ -0,0 +1,13 @@
+resource "hcp_hvn" "ars_hvn" {
+  hvn_id         = "hvn"
+  cloud_provider = "aws"
+  region         = "ap-southeast-1"
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_vault_cluster" "ars_vault_cluster" {
+  cluster_id = "ars-vault-cluster"
+  hvn_id     = hcp_hvn.ars_hvn.hvn_id
+  tier       = "dev"
+  public_endpoint = true #to get vault cluster access from public
+}
