639cb4241a47685216c9a757664126471fedd98a
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-18 15:06:04 +0000

    add terraform files

--- vault_jwt_auth_backend/outputs.tf
+++ vault_jwt_auth_backend/outputs.tf
@@ -1,5 +1,5 @@
 output "role_name" {
   description = "Name of the auth role"
-  value       = vault_jwt_auth_backend_role.jwt_auth_role.role_name
+  value       = vault_jwt_auth_backend_role.jwt_admin_role.role_name
 }
 
