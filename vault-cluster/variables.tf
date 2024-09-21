a7a6a4e4a5a2e76f51071e78c3230bb0c63b9781
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-21 13:40:03 +0800

    test /vault-cuslter/

--- vault-cluster/variables.tf
+++ vault-cluster/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
-  description = "AWS Region ap-southeast-6 for vault-cluster change to /vault-cuslter/"
+  description = "AWS Region ap-southeast-6 for vault-cluster change to /vault-cuslter/*.tf"
   default     = "ap-southeast-1"
 }
