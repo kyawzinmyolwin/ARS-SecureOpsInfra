7a791df3763b9d04ae9963e99f630e2fc0376850
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-21 11:07:44 +0800

    all vcs /modules

--- vault_jwt_auth_backend/outputs.tf
+++ vault_jwt_auth_backend/outputs.tf
@@ -1,5 +1,5 @@
 output "role_name" {
-  description = "Name of the auth role"
+  description = "Name of the auth role all vcs /modules"
   value       = vault_jwt_auth_backend_role.jwt_admin_role.role_name
 }
 
