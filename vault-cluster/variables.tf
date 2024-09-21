a62753814eb97d7bfb1c3fc1e043c7615415854b
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-21 11:08:46 +0800

    all vcs /modules

--- vault-cluster/variables.tf
+++ vault-cluster/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
-  description = "AWS Region ap-southeast-6 for vault-cluster add *.tf 2"
+  description = "AWS Region ap-southeast-6 for vault-cluster all vcs /modules"
   default     = "ap-southeast-1"
 }
