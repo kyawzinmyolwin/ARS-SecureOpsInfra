60263d53c493bdd72b12f4a4368ba5e4b67a98ac
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-21 10:49:00 +0800

    testing vcs trigger path 11

--- vault-cluster/variables.tf
+++ vault-cluster/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
-  description = "AWS Region ap-southeast-6 for vault-cluster"
+  description = "AWS Region ap-southeast-6 for vault-cluster 11"
   default     = "ap-southeast-1"
 }
