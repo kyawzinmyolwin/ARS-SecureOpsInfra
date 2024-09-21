f9e254b8f0180c159b2bcb6cf035d6e772266c02
kyawzinmyolwin <kyawzin.workspace@gmail.com>
2024-09-21 11:05:26 +0800

    all vcs /modules

--- test-jwt-auth-role/outputs.tf
+++ test-jwt-auth-role/outputs.tf
@@ -3,6 +3,6 @@ output "secret_name" {
   value       = vault_kv_secret_v2.example.name
 }
 output "secret_path" {
-  description = "Path to the secret"
+  description = "Path to the secret test vcs"
   value       = vault_kv_secret_v2.example.path
 }
